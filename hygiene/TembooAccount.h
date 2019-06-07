2489dc9bf35bdd43e24bb615bb3e096e3fbda4ae
15EC049 <umarbasheer1996@gmail.com>
2019-06-07 08:50:19 +0530

    temboo credintial

---
+++ hygiene/TembooAccount.h
@@ -0,0 +1,7 @@
+#define TEMBOO_ACCOUNT "hygiene"  // Your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app key name
+#define TEMBOO_APP_KEY "W7agfGex2aDh3mERfSM7c5oLdjsCDBNt"  // Your Temboo app key
+#define TEMBOO_DEVICE_TYPE "CC3220S"
+
+#define WIFI_SSID "Umar"
+#define WPA_PASSWORD "umarahammed"
